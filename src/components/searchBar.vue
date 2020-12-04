51986a930ec035fff5aa65907bb3c8961c9a2871
RhinoLee <picklerhino@gmail.com>
2020-12-04 21:16:19 +0800

    add cityCountryData & select option

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,15 +1,103 @@
 <template>
   <div>
-    
+    <pre>{{ currentCountry }}</pre>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+
+          <div class="is-multiple">
+            <div class="select">
+              <select v-model="currentCity">
+                <option value="null">縣市</option>
+                <option 
+                v-for="city in CityCountyData" 
+                :key="city.CityName"
+                >
+                {{ city.CityName }}
+                </option>
+              </select>
+            </div>
+          </div>
+          
+          <div class="is-multiple">
+            <div class="select">
+              <select v-model="currentArea">
+                <option value="null">鄉鎮</option>
+                <option 
+                v-for="area in areas" 
+                :key="area.AreaName"
+                >
+                {{ area.AreaName }}
+                </option>
+              </select>
+            </div>
+          </div>
+          
+
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <!-- navbar items -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  watch: {
+    currentCity(){
+      this.currentArea = 'null'
+    }
+  },
+  computed: {
+    CityCountyData(){
+      return this.$store.state.CityCountyData
+    },
+    currentCity: {
+      get(){
+        return this.$store.getters.currentCity
+      },
+      set(val){
+        this.$store.commit('setCurrentCity', val)
+      }
+    },
+    areas(){
+      return this.$store.getters.areas
+    },
+    currentArea: {
+      get(){
+        return this.$store.getters.currentArea
+      },
+      set(val){
+        this.$store.commit('setCurrentArea', val)
+      }
+    }
+  }
 }
 </script>
 
-<style>
+
+<style lang="scss" scoped>
+.navbar-menu {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
 
 </style>
+
+
+[
+  {
+    "CityName": "臺北市",
+    "CityEngName": "Taipei City",
+    "AreaList": [
+      {
+        "ZipCode": "100",
+        "AreaName": "中正區",
+        "AreaEngName": "Zhongzheng Dist."
+      },
+    ]
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,49 @@
 import { createStore } from 'vuex'
 const GasStationData = require('../../public/data/service_data.json')
+const CityCountyData = require('../../public/data/CityCountyData.json')
 const maxPagesCount = 15
 const countOfPage = 15
 
 export default createStore({
   state: {
     GasStationData,
+    CityCountyData,
     currentPage: 1,
+    currentCity: 'null',
+    areas: [],
+    currentArea: 'null'
   },
   mutations: {
     setCurrentPage(state, payload){
       state.currentPage = payload
-    }
+    },
+    setCurrentCity(state, payload){
+      state.currentCity = payload
+    },
+    setCurrentArea(state, payload){
+      state.currentArea = payload
+    },
+
   },
   getters: {
+    currentCity(state){
+      return state.currentCity
+    },
+    areas(state, getters){
+      let arr = []
+
+      state.CityCountyData.forEach(city => {
+        if(city.CityName === getters.currentCity) {
+          arr = city.AreaList
+        }
+      })
+
+      return arr
+     
+    },
+    currentArea(state){
+      return state.currentArea
+    },
     totalPages(state){
       return Math.ceil(state.GasStationData.length / countOfPage)
     }, 
